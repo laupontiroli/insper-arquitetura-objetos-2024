09a4c26ed035c0922c3b7aaef800d5a50ab987a6
Eduardo Santana <ezambomsantana@gmail.com>
2024-02-29 12:25:54 -0300

    add aula-7

---
+++ aula07/aula-07/src/main/java/br/insper/ecommerce/pagamento/Pix.java
@@ -0,0 +1,35 @@
+package br.insper.ecommerce.pagamento;
+
+import java.time.LocalDateTime;
+
+public class Pix extends MeioPagamento {
+    private String chaveOrigem;
+    private String qrCode;
+
+    public Pix() {
+
+    }
+
+    public Pix(boolean aprovado, LocalDateTime dataAprovacao, String chaveOrigem, String qrCode) {
+        super(aprovado, dataAprovacao);
+        this.chaveOrigem = chaveOrigem;
+        this.qrCode = qrCode;
+
+    }
+
+    public String getChaveOrigem() {
+        return chaveOrigem;
+    }
+
+    public void setChaveOrigem(String chaveOrigem) {
+        this.chaveOrigem = chaveOrigem;
+    }
+
+    public String getQrCode() {
+        return qrCode;
+    }
+
+    public void setQrCode(String qrCode) {
+        this.qrCode = qrCode;
+    }
+}
